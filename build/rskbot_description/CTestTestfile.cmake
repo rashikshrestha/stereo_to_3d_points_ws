ba028ddf55e71db1c4a06b2a5b470cf352ad4cc2
rashikstha <073bex432.rashik@pcampus.edu.np>
2020-03-27 18:53:42 +0545

    removed folders

--- build/rskbot_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rashik/ros/stereo_to_3d_points_ws/src/rskbot_description
-# Build directory: /home/rashik/ros/stereo_to_3d_points_ws/build/rskbot_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
